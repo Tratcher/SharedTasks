9052d736f2f149360822cc7333d1f297d0591300
Chris Ross <Tratcher@Outlook.com>
2020-05-02 15:49:28 -0700

    Update RecurringTaskScheduler.ps1

--- .github/workflows/RecurringTaskScheduler.ps1
+++ .github/workflows/RecurringTaskScheduler.ps1
@@ -1,1 +1,4 @@
-
+# Query for closed recurring issues
+$Headers = @{ Authorization = 'token {0}' -f $ENV:GITHUB_TOKEN; };
+$result = Invoke-RestMethod -Headers $Headers -Uri https://api.github.com/repos/Tratcher/SharedTasks/issues?state=closed&labels=Recurring
+echo $result
