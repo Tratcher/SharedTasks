5a3a7531c1a9f07efbd06d1d4328d8fd0510ea8b
Chris Ross <Tratcher@Outlook.com>
2020-05-03 10:44:07 -0700

    Update RecurringTaskScheduler.ps1

--- .github/workflows/RecurringTaskScheduler.ps1
+++ .github/workflows/RecurringTaskScheduler.ps1
@@ -1,90 +1,131 @@
+$todaysDate = [System.DateTime]::Now.Date
+"Today's Date: " + $todaysDate
+
 # Query for closed recurring issues
 $Headers = @{ Authorization = 'token {0}' -f $ENV:GITHUB_TOKEN; };
 $result = Invoke-RestMethod -Headers $Headers -Uri "https://api.github.com/repos/Tratcher/SharedTasks/issues?state=closed&labels=Recurring"
-$todaysDate = [System.DateTime]::Now.Date
-$todaysDate
+
+# Parse each issue
 ForEach ($item in $result)
 {
- $item.title
+ "Title: " + $item.title
  $closedDate = [System.DateTimeOffset]::Parse($item.closed_at).Date
- $closedDate
- $lines = $item.body.Split("`r`n", [System.StringSplitOptions]::RemoveEmptyEntries);
- $lines
- $type = "";
+ "Closed at: " + $closedDate
+ 
  $every = "";
+ $unit = "";
+ $sense = "";
  $on = "";
- for ($i = 0; $i -lt $lines.Count; $i++)
+ $reader = New-Object -TypeName System.IO.StringReader -ArgumentList $item.body
+ while ($True)
  {
-   $line = $lines[$i];
-   if ($line.Equals("#### Recurrence Schedule"))
-   {
-     if (($i + 1) -lt $lines.Count)
-     {
-       $typeLineWords = $lines[$i + 1].Split(' ');
-       if ($typeLineWords.Count -gt 1 -and [System.String]::Equals($typeLineWords[0], "Type:", [System.StringComparison]::OrdinalIgnoreCase))
-       {
-         $type = $typeLineWords[1];
-       }
-     }
+  $line = $reader.ReadLine();
 
-     if (($i + 2) -lt $lines.Count)
-     {
-       $everyLineWords = $lines[$i + 2].Split(' ');
-       if ($everyLineWords.Count -gt 1 -and [System.String]::Equals($everyLineWords[0], "Every:", [System.StringComparison]::OrdinalIgnoreCase))
-       {
-         $every = $everyLineWords[1];
-       }
-     }
+  if ($line -eq $null)
+  {
+      break;
+  }
 
-     if (($i + 3) -lt $lines.Count)
-     {
-       $onLineWords = $lines[$i + 3].Split(' ');
-       if ($onLineWords.Count -gt 1 -and [System.String]::Equals($onLineWords[0], "On:", [System.StringComparison]::OrdinalIgnoreCase))
-       {
-         $on = $onLineWords[1];
-       }
-     }
-     break;
-   }
+  if ($line.Equals("#### Recurrence Schedule"))
+  {
+    # Every
+    $line = $reader.ReadLine();
+    if ($line -eq $null)
+    {
+        break;
+    }
+
+    $everyLineWords = $line.Split(' ');
+    if ($everyLineWords.Count -gt 1 -and [System.String]::Equals($everyLineWords[0], "Every:", [System.StringComparison]::OrdinalIgnoreCase))
+    {
+        $every = $everyLineWords[1];
+    }
+    
+    # Unit
+    $line = $reader.ReadLine();
+    if ($line -eq $null)
+    {
+        break;
+    }
+
+    $unitLineWords = $line.Split(' ');
+    if ($unitLineWords.Count -gt 1 -and [System.String]::Equals($unitLineWords[0], "Unit:", [System.StringComparison]::OrdinalIgnoreCase))
+    {
+        $unit = $unitLineWords[1];
+    }
+    
+    # Since
+    $line = $reader.ReadLine();
+    if ($line -eq $null)
+    {
+        break;
+    }
+
+    $sinceLineWords = $line.Split(' ');
+    if ($sinceLineWords.Count -gt 1 -and [System.String]::Equals($sinceLineWords[0], "Since:", [System.StringComparison]::OrdinalIgnoreCase))
+    {
+        $since = $sinceLineWords[1];
+    }
+
+    # On
+    $line = $reader.ReadLine();
+    if ($line -eq $null)
+    {
+        break;
+    }
+
+    $onLineWords = $line.Split(' ');
+    if ($onLineWords.Count -gt 1 -and [System.String]::Equals($onLineWords[0], "On:", [System.StringComparison]::OrdinalIgnoreCase))
+    {
+        $on = $onLineWords[1];
+    }
+    
+    break;
+  }
  }
+ $every
+ $unit
+ $since
+ $on
 
  $reopen = false
- if ([System.String]::Equals($type, "Daily", [System.StringComparison]::OrdinalIgnoreCase))
+ if ([System.String]::Equals($unit, "Days", [System.StringComparison]::OrdinalIgnoreCase))
  {
-    $type
-    "Reopen"
+    $every
+    $unit
+    $since
     $reopen = $True
  }
- elseif ([System.String]::Equals($type, "Weekly", [System.StringComparison]::OrdinalIgnoreCase))
+ elseif ([System.String]::Equals($unit, "Weeks", [System.StringComparison]::OrdinalIgnoreCase))
  {
-    $type
     $every
+    $unit
+    $since
     $on
     if ($todaysDate.DayOfWeek -eq $on)
     {
-      "Reopen"
       $reopen = $True
     }
  }
- elseif ([System.String]::Equals($type, "Monthly", [System.StringComparison]::OrdinalIgnoreCase))
+ elseif ([System.String]::Equals($unit, "Months", [System.StringComparison]::OrdinalIgnoreCase))
  {
-    $type
     $every
+    $unit
+    $since
     $on
     if ($todaysDate.Day -eq $on)
     {
-      "Reopen"
       $reopen = $True
     }
  }
  else
  {
-   $message = "Unrecognized type: " + $type
-   $message
+   "Unrecognized unit: " + $unit
  }
  $reopen
  if ($reopen)
  {
+  "Reopen"
   $json = "{ `"state`": `"open`" }"
   $result = Invoke-RestMethod -Method PATCH -Headers $Headers -Uri $item.url -Body $json
   $result
